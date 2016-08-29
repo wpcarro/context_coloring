8d53f62659ed7f8a6b4edb1462e5ad589e6757a6
William Carroll <wcarroll@hugeinc.com>
2016-08-28 22:46:15 -0400

    Removes support for bg highlighting

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import fs from 'fs';
 
 // view "https://www.npmjs.com/package/colors" for a list of supported colors
 const nestingLevelToFgBg = {
-  ['-1']: {fg: 'black', bg: 'bgRed'},
-  ['0']: {fg: 'black', bg: 'bgGreen'},
-  ['1']: {fg: 'black', bg: 'bgYellow'},
-  ['2']: {fg: 'black', bg: 'bgBlue'},
-  ['3']: {fg: 'black', bg: 'bgMagenta'},
-  ['4']: {fg: 'black', bg: 'bgCyan'},
-  ['5']: {fg: 'black', bg: 'bgWhite'}
+  ['-1']: {fg: 'white'},
+  ['0']: {fg: 'green'},
+  ['1']: {fg: 'yellow'},
+  ['2']: {fg: 'blue'},
+  ['3']: {fg: 'magenta'},
+  ['4']: {fg: 'cyan'},
+  ['5']: {fg: 'gray'}
 };
 
 
@@ -46,10 +46,9 @@ const contextRanges = eslevels.levels(syntax, {
 
 
 function colorize(color, string) {
-  const {fg, bg} = color;
+  const { fg } = color;
 
-  return ansiStyles[fg].open + ansiStyles[bg].open +
-      string + ansiStyles[fg].close + ansiStyles[bg].close;
+  return ansiStyles[fg].open + string + ansiStyles[fg].close;
 }
 
 
