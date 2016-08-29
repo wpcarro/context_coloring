a3fd39b17d486273578506d19c9729becfa60690
William Carroll <wcarroll@hugeinc.com>
2016-08-28 23:12:47 -0400

    Refactors dir paths to reflect package dir

--- context_coloring.sh
+++ context_coloring.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 node_bin=$(which node)
-babel="./node_modules/.bin/babel"
+pkg_dir="/usr/local/bin/context_coloring"
+babel="${pkg_dir}/node_modules/.bin/babel"
 
 
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 
-if [ ! -f "./index.out.js" ]; then
+if [ ! -f "${pkg_dir}/index.out.js" ]; then
   echo "mmm no"
-  "${babel}" ./index.js --out-file ./index.out.js
+  "${babel}" "${pkg_dir}/index.js" --out-file "${pkg_dir}/index.out.js"
 fi
 
-"${node_bin}" ./index.out.js "${input}"
+"${node_bin}" "${pkg_dir}/index.out.js" "${input}"
 
 
